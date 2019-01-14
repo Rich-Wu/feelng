37fa8514bee87cdc37dbaa6bd2b3c01ed0d8e22f
Momo Khan <snillmomo@gmail.com>
2019-01-14 17:17:57 -0500

    Added getting all the categories. Add get playlists. Add web player.

--- app/services/music_recommendations.rb
+++ app/services/music_recommendations.rb
@@ -1,22 +1,59 @@
 require 'httparty'
-require 'base64'
+require 'json'
 
 class MusicRecommendations 
-    
+    include HTTParty
+    format :json
+
     def initialize
+        id = ENV["SPOTIFY_ID"]
+        secret = ENV["SPOTIFY_SECRET"]
+        credentials = "#{id}:#{secret}"
+        # encode the string to base 64
+        enc = Base64.encode64(credentials)
+        enc = enc.gsub(/\s+/,"")
+
         body = {
             "grant_type" => "client_credentials"
         }
-        credentials = "60a9a07eff2c457ab881003fd346e59a:244ee2cf31e447018353cbed875dce5c"
-        enc = Base64.encode64(credentials)
-        enc = enc.gsub(/\s+/,"")
-        puts enc
+        
         headers = {
             "Authorization" => "Basic #{enc}"
         }
+        # make the post request with required body and header parameters
         response = HTTParty.post("https://accounts.spotify.com/api/token", :body => body, :headers => headers)     
-        puts response.body, response.code, response.message, response.headers.inspect
-
+        body = JSON.parse(response.body)
+        # set the access token for future calls
+        @access_token = body["access_token"]
     end
 
+    def get_categories
+        categories = []
+        query = {
+            "limit" => 50,
+            "offset" => 0
+        }
+
+        headers = {
+            "Authorization" => "Bearer #{@access_token}"
+        }
+        response = HTTParty.get("https://api.spotify.com/v1/browse/categories", :query => query, :headers => headers)
+        body = JSON.parse(response.body)
+        items = body["categories"]["items"]
+        items.each do |item| 
+            categories << item["id"]
+        end
+        puts categories
+    end
+
+    def get_category_playlist(category_id) 
+        query = {
+            "country" => "US",
+            "limit" => 50
+        }
+        response = HTTParty.get("https://api.spotify.com/v1/browse/categories/#{category_id}/playlists")
+        body = JSON.parse(response.body)
+        playlists = body["playlists"]
+        
+    end
 end
