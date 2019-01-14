e9db7f9d1b4367514e2122f4c05b567698990bd8
Richard <richie1988@gmail.com>
2019-01-14 12:17:15 -0500

    curb gemfile.lock

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.7'
 
+# Use Curl for HTTP requests
+gem 'curb'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'net/http'
+require 'uri'
+require 'curb'
+require 'json'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,4 +20,27 @@ module Groupproject
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+end
+
+def query_foursquare
+  uri = URI.parse("https://api.foursquare.com/v2/venues/explore")
+  request = Net::HTTP::Get.new(uri)
+  request.set_form_data(
+    "client_id" => ENV['FOURSQ_ID'],
+    "client_secret" => ENV['FOURSQ_KEY'],
+    "ll" => "40.7243,-74.0018",
+    "query" => "coffee",
+    "v" => "20180323",
+  )
+
+  req_options = {
+    use_ssl: uri.scheme == "https",
+  }
+
+  response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+
+  Curl::Easy.http_get()
+
 end
