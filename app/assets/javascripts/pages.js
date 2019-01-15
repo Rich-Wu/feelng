76389aba82b0da2c272bfafbe76184441946846c
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-15 11:39:26 -0500

    adjusting the menu section approach...now creating a dropdown with links to specific overlays

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,9 +1,9 @@
-var menu_overlay = document.getElementById('menu-overlay');
-
-function openOverlay() {
-  menu_overlay.style.height = "100%";
-}
-
-function closeOverlay() {
-  menu_overlay.style.height = "0";
-}
+// var menu_overlay = document.getElementById('menu-overlay');
+//
+// function openOverlay() {
+//   menu_overlay.style.height = "100%";
+// }
+//
+// function closeOverlay() {
+//   menu_overlay.style.height = "0";
+// }
