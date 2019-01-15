f21517a3894a7dcf80d34baf0d9fc8470c40f861
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-15 15:25:13 -0500

    created separate overlays per menu item + styling

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,28 +1,54 @@
 var menu_content = document.getElementById('menu-content');
-var home_overlay = document.getElementById('overlay');
 var menu_container = document.getElementsByClassName('menu-container')[0];
+var about_overlay = document.getElementById('overlay1');
+var login_overlay = document.getElementById('overlay2');
+var signup_overlay = document.getElementById('overlay3');
 
 // DISPLAYS MENU CONTENT
 function openMenu() {
   menu_content.classList.toggle('drop');
 }
 
-// DISPLAYS OVERLAY
-function openOverlay() {
-  home_overlay.style.height = "100%";
+// DISPLAYS ARRAY OF OVERLAYS
+function openOverlay1() {
+  about_overlay.style.height = "100%";
   menu_container.style.display = "none";
 }
 
-// CLOSES OVERLAY
-function closeOverlay() {
-  home_overlay.style.height = "0";
+function openOverlay2() {
+  login_overlay.style.height = "100%";
+  menu_container.style.display = "none";
+}
+
+function openOverlay3() {
+  signup_overlay.style.height = "100%";
+  menu_container.style.display = "none";
+}
+
+// CLOSES ARRAY OF OVERLAYS
+function closeOverlay1() {
+  about_overlay.style.height = "0";
+  setTimeout(function() {
+    menu_container.style.display = "block";
+  }, 500);
+}
+
+function closeOverlay2() {
+  login_overlay.style.height = "0";
+  setTimeout(function() {
+    menu_container.style.display = "block";
+  }, 500);
+}
+
+function closeOverlay3() {
+  signup_overlay.style.height = "0";
   setTimeout(function() {
     menu_container.style.display = "block";
   }, 500);
 }
 
 // CLOSES MENU CONTENT
-window.onclick = function(event) {
-  if (!event.target.matches('.menu-container'))
-    menu_content.classList.remove('drop');
-}
+// window.onclick = function(event) {
+//   if (event.target == menu_content)
+//     menu_content.style.display = "none";
+// }
