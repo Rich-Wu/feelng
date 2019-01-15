b9e2e62da7831324597f33bebdd3fd71617c35da
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-15 14:38:37 -0500

    overlay is functioning using menu links

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,12 +1,28 @@
 var menu_content = document.getElementById('menu-content');
+var home_overlay = document.getElementById('overlay');
+var menu_container = document.getElementsByClassName('menu-container')[0];
 
 // DISPLAYS MENU CONTENT
 function openMenu() {
   menu_content.classList.toggle('drop');
 }
 
+// DISPLAYS OVERLAY
+function openOverlay() {
+  home_overlay.style.height = "100%";
+  menu_container.style.display = "none";
+}
+
+// CLOSES OVERLAY
+function closeOverlay() {
+  home_overlay.style.height = "0";
+  setTimeout(function() {
+    menu_container.style.display = "block";
+  }, 500);
+}
+
 // CLOSES MENU CONTENT
-// window.onclick = function(event) {
-//   if (!event.target.matches('.menu-container'))
-//     menu_content.classList.remove('drop');
-// }
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-container'))
+    menu_content.classList.remove('drop');
+}
