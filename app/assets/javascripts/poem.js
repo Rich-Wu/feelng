93ed996a6b03e354f48789075a785a96cde215dd
Olivia Ellis <oaksellis@gmail.com>
2019-01-17 18:03:35 -0500

    continued styling. Home and results page have mobile-first responsive layouts

--- app/assets/javascripts/poem.js
+++ app/assets/javascripts/poem.js
@@ -2,7 +2,7 @@ function expand() {
   console.log('Clicked');
   poem = document.getElementById('poem');
   if (poem.style.height == '20vh') {
-    poem.style.height = '100%';
+    poem.style.height = '60vh';
   } else {
     poem.style.height = '20vh';
   }
