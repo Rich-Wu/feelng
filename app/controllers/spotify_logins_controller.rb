410db78a93c44fe1330a960af356ed6b5bf42f76
Momo Khan <snillmomo@gmail.com>
2019-01-21 20:28:43 -0500

    Adding correct redirect_uri for production

--- app/controllers/spotify_logins_controller.rb
+++ app/controllers/spotify_logins_controller.rb
@@ -4,11 +4,13 @@ class SpotifyLoginsController < ApplicationController
       client_id: ENV['SPOTIFY_ID'],
       response_type: 'code',
       # Replace the redirect URI to the url responsible for saving usrs tokens
-      redirect_uri: "http://localhost:3000/spotify_users/create",
       scope: "streaming user-read-birthdate user-read-email user-read-private user-modify-playback-state",
       show_dialog: true
     }
-
+    if Rails.env.production?
+      query_params[:redirect_uri] = "http://feelng.herokuapp.com/spotify_users/create"
+    else
+      query_params[:redirect_uri] = "http://localhost:3000/spotify_users/create"
     url = "https://accounts.spotify.com/authorize/"
     puts "#{url}?#{query_params.to_query}"
     redirect_to "#{url}?#{query_params.to_query}"
