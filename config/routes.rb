ac4fa50d847176c11e26c323c0f853411a8e7d33
Olivia Ellis <oaksellis@gmail.com>
2019-01-21 20:24:05 -0500

    fixed merge and stylesheet  conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   post '/logout', to: 'spotify_users#logout', as: 'logout'
+  # get '/entries/:id', to: 'entries#read', as: "entry"
   get 'entries/delete'
   mount ActionCable.server => '/cable'
   post '/messages', to: 'messages#create', as: 'new_message'
   get '/entries/:id', to: 'entries#show', as: "entry"
   get 'users/update'
+  post '/entries', to: 'entries#create', as: 'new_entry'
+  get '/profile', to: 'spotify_users#view', as: 'profile'
   get 'spotify_users/create'
   get 'spotify_logins/create'
   root 'pages#home'
